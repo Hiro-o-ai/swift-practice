39e360b625064a8a2b5040a0ceb8e1ea18837e2e
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-18 07:34:06 +0900

    start_janken_app

---
+++ MyJanken/MyJanken/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by 岡島宏忠 on 2020/09/17.
+//  Copyright © 2020 swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var answerImageView: UIImageView!
+}
+
