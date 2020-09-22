bd8818c0c999f70bdef90735cadffb1501be5af7
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-22 15:45:16 +0900

    リファクタリングMymusic

--- MyMusic/MyMusic/ViewController.swift
+++ MyMusic/MyMusic/ViewController.swift
@@ -21,11 +21,7 @@ class ViewController: UIViewController {
     var cymbalPlayer = AVAudioPlayer()
     
     @IBAction func cymbal(_ sender: Any) {
-        do { cymbalPlayer = try  AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
-        cymbalPlayer.play()
-        } catch {
-            print("シンバルで、エラーが発生しました！")
-        }
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
     }
     
     let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
@@ -33,11 +29,7 @@ class ViewController: UIViewController {
     var guitarPlayer = AVAudioPlayer()
     
     @IBAction func guitar(_ sender: Any) {
-        do { guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-            guitarPlayer.play()
-        } catch {
-            print("ギターで、エラーが発生しました！")
-        }
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
     }
     
     let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
@@ -45,15 +37,20 @@ class ViewController: UIViewController {
     var backmusicPlayer = AVAudioPlayer()
     
     @IBAction func play(_ sender: Any) {
-        do { backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
-            backmusicPlayer.numberOfLoops = -1
-            backmusicPlayer.play()
-        } catch {
-            print("エラーが発生しました！")
-        }
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
     }
     
     @IBAction func stop(_ sender: Any) {
         backmusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました！")
+        }
     }
 }
