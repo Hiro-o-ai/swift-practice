6a802aec07a99c8384379d64611178885b7c6226
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-25 11:01:43 +0900

    start_mytimer

---
+++ MyTimer/MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by 岡島宏忠 on 2020/09/25.
+//  Copyright © 2020 swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
