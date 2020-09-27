bfd7596aeac0b55d302ae3cc76f32dd39d948101
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-09-27 11:07:53 +0900

    finish_mycamera1

---
+++ MyCamera/MyCamera/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by 岡島宏忠 on 2020/09/26.
+//  Copyright © 2020 swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var pictureImage: UIImageView!
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print ("カメラは利用できます")
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = .camera
+            imagePickerController.delegate = self
+            present(imagePickerController, animated: true, completion: nil)
+        } else {
+            print ("カメラは利用できません")
+        }
+    }
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+        if let shareImage = pictureImage.image {
+            let shareItems = [shareImage]
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
+}
+
