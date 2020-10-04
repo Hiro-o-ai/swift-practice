cdfd834a82cba78558740585a65007c1fcb43fe4
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-10-04 21:36:55 +0900

    start_myokashi

---
+++ MyOkashi/MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by 岡島宏忠 on 2020/10/04.
+//  Copyright © 2020 swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
