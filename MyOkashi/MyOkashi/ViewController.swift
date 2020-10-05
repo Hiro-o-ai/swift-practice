8b4d1e532c512c5b356ea477d1530d03a207cffe
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-10-05 21:03:16 +0900

    puts_UIparts

--- MyOkashi/MyOkashi/ViewController.swift
+++ MyOkashi/MyOkashi/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchText.delegate = self
+        searchText.placeholder = "お菓子の名前を入力してください"
     }
 
 
+    @IBOutlet weak var searchText: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        if let searchWord = searchBar.text {
+            print(searchBar)
+        }
+    }
 }
 
