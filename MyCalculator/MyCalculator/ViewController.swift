5aa8d28ec35b350f63bb48bd8cf585f8808f7ecc
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-10-09 13:54:00 +0900

    initial_commit

---
+++ MyCalculator/MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by 岡島宏忠 on 2020/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
